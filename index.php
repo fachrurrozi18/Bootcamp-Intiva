7e2a50fe4f56d618ebd194a443e28899b652e24c
fachrurrozi18 <74090583+fachrurrozi18@users.noreply.github.com>
2022-12-16 20:32:21 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -7,16 +7,23 @@ echo "Tipe data adalah sebuah wadah yang menyimpan sebuah nilai dari setiap vari
 //Nomor 2
 echo "<br/>";
 echo "Nomor 2 <br/>";
-echo "Integer adalah sebuah tipe data yang menyimpan nilai dari angka - angka yang terdiri atas bilangan bulat <br/>";
-echo "String adalah sebuah tipe data yang menyimpan nilai dari sebuah kumpulan huruf yang disatukan menjadi sebuah kata <br/>";
-echo "Float adalah sebuah tipe data yang menyimpan nilai dari sebuah angka desimal seperti bilangan 0.1 <br/>";
-echo "Char adalah sebuah tipe data yang menyimpan nilai dari sebuah huruf - huruf yang membedakan dari string adalah ini pecahan dari setiap suku kata seperti A B C dan seterusnya <br/>";
-echo "Boolean adalah sebuah tipe data yang menyimpan nilai dari sebuah nilai kebenaran true dan false yang dinotasikan dengan bilangan biner 0(False) dan 1(True) <br/>";
+echo "Integer adalah sebuah menyimpan nilai dari angka - angka yang terdiri atas bilangan bulat <br/>";
+echo "String adalah sebuah menyimpan nilai dari sebuah kumpulan huruf yang disatukan menjadi sebuah kata <br/>";
+echo "Float adalah sebuah menyimpan nilai dari sebuah angka desimal <br/>";
+echo "Char adalah sebuah menyimpan nilai dari sebuah huruf abjad<br/>";
+echo "Boolean adalah sebuah menyimpan nilai dari sebuah nilai kebenaran true dan false yang dinotasikan dengan bilangan biner 0(False) dan 1(True) <br/>";
 
 //Nomor 3
 echo "<br/>";
 echo "Nomor 3 <br/>";
 echo "Aritmatika adalah sebuah proses perhitungan yang terdapat sebuah notasi perhitungan seperti +, -, <br/>";
+
+
+echo "Pengurangan 15 - 5: ";
+$a = 5;
+$b = 15;
+//pengurangan variabel a dengan variabel b
+echo $b - $a; echo "<br/>";
 
 //Nomor 4
 echo "<br/>";
@@ -26,18 +33,6 @@ $a = 3;
 $b = 29;
 //menjumlahkan variabel a dengan variabel b
 echo $a + $b; echo "<br/>";
-
-echo "Pengurangan 15 - 5: ";
-$a = 5;
-$b = 15;
-//pengurangan variabel a dengan variabel b
-echo $b - $a; echo "<br/>";
-
-echo "Perkalian 4 * 20: ";
-$a = 4;
-$b = 20;
-//perkalian variabel a dengan variabel b
-echo $a * $b; echo "<br/>";
 
 echo "Pembagian 20 / 5: ";
 $a = 20;
@@ -50,6 +45,12 @@ $a = 18;
 $b = 8;
 //mencari sisa bagi variabel a dengan variabel b
 echo $a % $b; echo "<br/>";
+
+echo "Perkalian 4 * 20: ";
+$a = 4;
+$b = 20;
+//perkalian variabel a dengan variabel b
+echo $a * $b; echo "<br/>";
 
 //Nomor 5
 echo "<br/>";
